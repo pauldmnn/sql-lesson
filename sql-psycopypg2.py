2795be0ffb6ab5473070a64985b354c564013de0
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-11-08 09:22:54 +0000

    Initial commit

---
+++ sql-psycopypg2.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+# Connect to Chenook databsae
+connection = psycopg2.connect(database="chinook")
+
+# Build a cursor object of the database
+cursor = connection.cursor()
+
+# fetch the results(multiple)
+results = cursor.fetchall()
+
+# Query 1 - Select all records from the "Artists" table
+cursor.execute('SELECT * FROM "Artist"' )
+
+# to fetch for results(single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print the results
+for result in results:
+    print(result)
